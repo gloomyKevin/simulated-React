7f5aa0dfc743d1e74689e544f66173e86032ca1c
gloomykevin <1293535916@qq.com>
2020-09-16 23:46:20 +0800

    improve

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 import { createElement, Component, render } from './simulated_react.js'
 
 class MyComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            a: 1,
+            b: 2
+        }
+    }
+
     render() {
         return <div>
             <h1>my component</h1>
+            <span>{this.state.a.toString()}</span>
             {this.children}
         </div>
     }
